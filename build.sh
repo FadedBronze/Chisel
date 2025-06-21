fa3b8cdea370f4345cf793668c244778e7970e2f
root <root@LAPTOP-SM31145Q.localdomain>
2025-06-20 23:55:06 -0400

    build works on linux

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
-# SDF_ttf
-cmake -S ./lib/modules/SDL_ttf -B ./lib/builds/SDL_ttf -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE="./build" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE="./build" -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE="./build" -DSDL2_LIBRARY="./lib/builds/SDL/build/SDL2.lib" -DSDL2_INCLUDE_DIR="./lib/modules/SDL/include" -DSDL2TTF_VENDORED="ON" -DSDL2TTF_SAMPLES="OFF" -DBUILD_SHARED_LIBS="OFF"
-./lib/modules/SDL_ttf/external/download.sh
-cmake --build ./lib/builds/SDL_ttf --config Release
-
-# SDL
-cmake --build ./lib/builds/SDL --config Release
-cmake -S ./lib/modules/SDL -B ./lib/builds/SDL -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE="./build" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE="./build" -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE="./build"
- 
-# freetype
-cmake --build ./lib/builds/freetype --config Release
-cmake -S ./lib/modules/freetype -B ./lib/builds/freetype -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE="./build" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE="./build" -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE="./build"
+# # SDL
+# cmake --build ./lib/builds/SDL --config Release
+# cmake -S ./lib/modules/SDL -B ./lib/builds/SDL 
+# 
+# # SDF_ttf
+# cmake -S ./lib/modules/SDL_ttf -B ./lib/builds/SDL_ttf -DSDL2_LIBRARY="./lib/builds/SDL/build/SDL2.lib" -DSDL2_INCLUDE_DIR="./lib/modules/SDL/include" -DSDL2TTF_VENDORED="ON" -DSDL2TTF_SAMPLES="OFF" -DBUILD_SHARED_LIBS="OFF"
+# ./lib/modules/SDL_ttf/external/download.sh
+# cmake --build ./lib/builds/SDL_ttf --config Release
+#  
+# # freetype
+# cmake --build ./lib/builds/freetype --config Release
+# cmake -S ./lib/modules/freetype -B ./lib/builds/freetype
 
 # GLFW
-cmake -S ./lib/modules/glfw -B ./lib/builds/glfw -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE="./build" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE="./build" -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE="./build" -DGLFW_BUILD_DOCS="OFF"
-cmake --build ./lib/builds/glfw --config Release
+# cmake -S ./lib/modules/glfw -B ./lib/builds/glfw -DGLFW_BUILD_DOCS="OFF" -DBUILD_SHARED_LIBS="ON"
+# cmake --build ./lib/builds/glfw --config Release
+mv ./lib/builds/glfw/src/libglfw.so ./zig-out/bin/libglfw.so
--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ const builtin = @import("builtin");
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(std.Build.StandardTargetOptionsArgs{
         .default_target = .{
-            .abi = if (builtin.os.tag == .windows) .msvc else .gnu,
+            .abi = if (builtin.os.tag == .windows) .msvc else null,
         },
     });
 
@@ -26,7 +26,7 @@ pub fn build(b: *std.Build) !void {
     exe.addIncludePath(std.Build.LazyPath{ .cwd_relative = "lib/modules/freetype/include" });
     exe.addIncludePath(std.Build.LazyPath{ .cwd_relative = "lib/modules/freetype/include" });
     exe.addIncludePath(std.Build.LazyPath{ .cwd_relative = "lib/modules/glfw/include" });
-    
+
     // link C
     exe.linkSystemLibrary("c");
 
@@ -50,31 +50,42 @@ pub fn build(b: *std.Build) !void {
             exe.linkSystemLibrary("dxguid");
         },
         .linux => {
-            exe.linkSystemLibrary("X11");
-            exe.linkSystemLibrary("Xrandr");
-            exe.linkSystemLibrary("Xi");
             exe.linkSystemLibrary("GL");
-            exe.linkSystemLibrary("dl");
-            exe.linkSystemLibrary("pthread");
-            exe.linkSystemLibrary("m");
+            exe.linkSystemLibrary("z");
+            exe.linkSystemLibrary("png");
+            exe.linkSystemLibrary("brotlidec");
+
+            exe.addRPath(.{ .cwd_relative = "zig-out/bin" });
+            // need to dynamically link because of conflict for now
+            exe.linkSystemLibrary("glfw3");
         },
-        else => return error.UnsupportedPlatform
+        else => return error.UnsupportedPlatform,
     }
 
-    exe.addCSourceFile(.{ 
-        .file = std.Build.LazyPath{ .cwd_relative = "lib/stb_image_write.c", }, 
+    exe.addCSourceFile(.{
+        .file = std.Build.LazyPath{
+            .cwd_relative = "lib/stb_image_write.c",
+        },
         .flags = &[_][]const u8{"-std=c99"},
     });
 
-    exe.addCSourceFile(.{ 
-        .file = std.Build.LazyPath{ .cwd_relative = "lib/glad/src/glad.c", }, 
-        .flags = &[_][]const u8{"-std=c99"}, 
+    exe.addCSourceFile(.{
+        .file = std.Build.LazyPath{
+            .cwd_relative = "lib/glad/src/glad.c",
+        },
+        .flags = &[_][]const u8{"-std=c99"},
     });
 
-    exe.addObjectFile(.{ .cwd_relative = "lib/builds/SDL_ttf/build/SDL2_ttf.lib" });
-    exe.addObjectFile(.{ .cwd_relative = "lib/builds/freetype/build/freetype.lib" });
-    exe.addObjectFile(.{ .cwd_relative = "lib/builds/SDL/build/SDL2-static.lib" });
-    exe.addObjectFile(.{ .cwd_relative = "lib/builds/glfw/src/build/glfw3.lib" });
+    if (builtin.os.tag == .windows) {
+        exe.addObjectFile(.{ .cwd_relative = "lib/builds/SDL_ttf/build/SDL2_ttf.lib" });
+        exe.addObjectFile(.{ .cwd_relative = "lib/builds/freetype/build/freetype.lib" });
+        exe.addObjectFile(.{ .cwd_relative = "lib/builds/SDL/build/SDL2-static.lib" });
+        exe.addObjectFile(.{ .cwd_relative = "lib/builds/glfw/src/build/glfw3.lib" });
+    } else if (builtin.os.tag == .linux) {
+        exe.addObjectFile(.{ .cwd_relative = "lib/builds/SDL_ttf/libSDL2_ttf.a" });
+        exe.addObjectFile(.{ .cwd_relative = "lib/builds/freetype/libfreetype.a" });
+        exe.addObjectFile(.{ .cwd_relative = "lib/builds/SDL/libSDL2.a" });
+    } else return error.UnsupportedPlatform;
 
     const zm = b.dependency("zm", .{
         .target = target,
